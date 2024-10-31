0b9ff1d06f4e0706c23a3ba3ac00deb7fd4cc618
gabes <164550314+gabreeeeeeeeeees@users.noreply.github.com>
2024-10-31 17:22:52 -0300

    aterei uns bglh

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 #definindo a rota principal so site
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/login')
@@ -13,15 +13,15 @@ def login():
     return render_template('login.html')
 
 @app.route('/cadastro')
-def login():
+def cadastro():
     return render_template('cadastro.html')
 
 @app.route('/pagina')
-def login():
+def pagina():
     return render_template('pagina.html')
 
 @app.route('/perfil')
-def login():
+def perfil():
     return render_template('perfil.html')
 
 
