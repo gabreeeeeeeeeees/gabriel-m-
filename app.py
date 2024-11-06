be2cc2017195326d8f9edf36d5b82bc8f288767b
gabes <164550314+gabreeeeeeeeeees@users.noreply.github.com>
2024-11-06 16:33:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 #importamdo a biblioteca so Flask para fazer um si 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = "chave_muito_segura"
+
+#Criar uma lista de usário 
+usuarios = {
+    "admin": "admin 1234", 
+
+}
 
 app = Flask(__name__)
 
@@ -24,12 +33,12 @@ def pagina():
 def perfil():
     return render_template('perfil.html')
 
+@app.route('/primeira pagina')
+def primeira_pagina():
+    return render_template('primeira pagina.html')
 
-# Parte principal do programa em Phyton
-if __name__=='__main__':
-    app.run(debug=True)
 
-    # VERFIFICAR O LOGIN
+
 @app.route('/verificar-login', methods=['POST'])
 def verificar_login():
 # Pegando o que o usuário digitou no campo de entrada de user e senha
@@ -39,12 +48,11 @@ def verificar_login():
     # Verifica se o usuario digitado está na lista e se 
     # a senha está certa
     if username in usuarios and usuarios[username] == password:
-        return f"Bem-vindo, {username}!"
+        return redirect(url_for('primeira_pagina'))
     else:
-        return "Usuário ou senha inválidos."
-
-
-
+    #Flash envia mensagem para o front-end
+        flash('Usuário ou senha inscorretos', 'danger')
+        return redirect(url_for('login'))
 
 
 
